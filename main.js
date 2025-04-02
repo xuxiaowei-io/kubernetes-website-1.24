cb29ed8e6325d284123824ae3595a6ef9021b82c
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2025-04-02 23:52:33 +0800

    :sparkles: 日志

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
 import { app, BrowserWindow } from 'electron'
 import path from 'path'
 import { fileURLToPath } from 'node:url'
+import log from 'electron-log'
 
 const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
 console.log('Hello from Electron 👋')
+
+// 日志文件名，默认日志位置：
+// Windows 开发：C:\Users\%USERPROFILE%\AppData\Roaming\Electron\logs
+// Windows 安装：C:\Users\%USERPROFILE%\AppData\Roaming\项目名称\logs
+// macOS 开发：/Users/$USER/Library/Logs/Electron
+// macOS 安装：/Users/$USER/Library/Logs/项目名称
+// Linux 开发：~/.config/Electron/logs
+// Linux 运行 *.AppImage：~/.config/项目名称/logs
+// Linux 安装 *.deb：~/.config/项目名称/logs
+// Linux 安装 *.snap：~/snap/项目名称/x1/.config/项目名称/logs
+log.transports.file.fileName = 'main.log'
+log.scope.defaultLabel = 'main'
+log.scope.labelPadding = 8
+
+log.info('Hello from Electron 👋')
 
 const createWindow = () => {
   const mainWindow = new BrowserWindow({

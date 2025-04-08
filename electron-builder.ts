e7686eedfdbbd77410ceec24df980b1f00d1b878
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2025-04-08 08:05:37 +0800

    :bulb: 更新注释

--- electron-builder.ts
+++ electron-builder.ts
@@ -1,13 +1,14 @@
 import { Configuration } from 'electron-builder'
 
+// 导出默认的配置对象，类型为 electron-builder 的 Configuration
 export default <Configuration>{
-  // 应用唯一标识
+  // 应用唯一标识，使用环境变量 npm_package_name 动态生成
   appId: `cn.com.xuxiaowei.${process.env.npm_package_name}`,
-  // 版权信息
+  // 版权信息，包含作者名和网站链接
   copyright: 'Copyright © 徐晓伟 https://xuxiaowei.com.cn',
-  // 打包产物名称
+  // 打包产物名称，使用模板字符串动态生成，包含产品名、版本、平台和架构
   artifactName: '${productName}-${version}-${platform}-${arch}.${ext}',
-  // 使用 asar 打包
+  // 使用 asar 打包，将所有文件打包成一个 asar 归档文件
   asar: true,
   // asar 打包后解压的文件（运行前会自动解压）
   asarUnpack: [],
